4ea5c005862bdc95105cb478ae5082b086e83391
Diego Comfort <diegocomfort@gmail.com>
2023-09-06 21:08:51 -0700

    I worked on stuff

--- BubblesLab/src/bubble.ts
+++ BubblesLab/src/bubble.ts
@@ -1,20 +1,20 @@
-import Vec2D from "./vec2d";
+import Vec2D from "./vec2d.js";
 
-export type VectorCallback = (pos: Vec2D, vel: Vec2D, acc: Vec2D) => [pos: Vec2D, vel: Vec2D, acc: Vec2D];
+export type UpdateCallback = (bubble: Bubble) => void;
 
 export default class Bubble {
     private radius: number;
     private pos: Vec2D;
     private vel: Vec2D;
     private acc: Vec2D;
-    private isOverlapping: boolean = false;
-    private color: (isOverlapping: boolean) => string;
+    private overlapping: boolean = false;
+    private color: string;
 
     constructor(radius: number,
                 pos: Vec2D = new Vec2D(), 
                 vel: Vec2D = new Vec2D(), 
                 acc: Vec2D = new Vec2D(),
-                color: (isOverlapping: boolean) => string = (isOverlapping) => isOverlapping ? "white": "black") {
+                color: string = "black") {
         this.radius = radius;
         this.pos = pos;
         this.vel = vel;
@@ -22,41 +22,55 @@ export default class Bubble {
         this.color = color;
     }
 
-    update(updateFunction: VectorCallback) {
-        [this.pos, this.vel, this.acc] = updateFunction(this.pos, this.vel, this.acc);
+    update(updateFunction: UpdateCallback) {
+        updateFunction(this);
     }
 
-    static UPDATE_POS = (pos: Vec2D, vel: Vec2D, acc: Vec2D): [pos: Vec2D, vel: Vec2D, acc: Vec2D] => {
-        vel = vel.add(acc);
-        pos = pos.add(vel);
-        return [pos, vel, acc];
+    static UPDATE_POS = (bubble: Bubble): void => {
+        bubble.vel = bubble.vel.add(bubble.acc);
+        bubble.pos = bubble.pos.add(bubble.vel);
     }
 
-    static CHECK_WALLS = (width: number, height: number): VectorCallback => {
-        return (pos: Vec2D, vel: Vec2D, acc: Vec2D): [pos: Vec2D, vel: Vec2D, acc: Vec2D] => {
-            if (pos.x < 0) {
-                pos = new Vec2D(0, pos.y);
-                vel = new Vec2D(-vel.x, vel.y);
-            }else if (pos.x > width) {
-                pos = new Vec2D(width, pos.y);
-                vel = new Vec2D(-vel.x, vel.y);
+    static CHECK_WALLS = (width: number, height: number): UpdateCallback => {
+        return (bubble: Bubble): void => {
+            if (bubble.pos.x - bubble.radius < 0) {
+                bubble.pos = new Vec2D(0, bubble.pos.y);
+                bubble.vel = new Vec2D(-bubble.vel.x, bubble.vel.y);
+            }else if (bubble.pos.x + bubble.radius > width) {
+                bubble.pos = new Vec2D(width, bubble.pos.y);
+                bubble.vel = new Vec2D(-bubble.vel.x, bubble.vel.y);
             }
-            if (pos.y < 0) {
-                pos = new Vec2D(pos.x, 0);
-                vel = new Vec2D(vel.x, -vel.y);
-            } else if (pos.y > height) {
-                pos = new Vec2D(pos.x, height);
-                vel = new Vec2D(vel.x, -vel.y);
+            if (bubble.pos.y - bubble.radius < 0) {
+                bubble.pos = new Vec2D(bubble.pos.x, 0);
+                bubble.vel = new Vec2D(bubble.vel.x, -bubble.vel.y);
+            } else if (bubble.pos.y + bubble.radius > height) {
+                bubble.pos = new Vec2D(bubble.pos.x, height);
+                bubble.vel = new Vec2D(bubble.vel.x, -bubble.vel.y);
             }
-            return [pos, vel, acc];
         };
+    }
+
+    static SET_OVERLAPPING_TRUE = (bubble: Bubble): void => {
+        bubble.overlapping = true;
+    }
+
+    static SET_OVERLAPPING_FALSE = (bubble: Bubble): void => {
+        bubble.overlapping = false;
+    }
+    static SET_COLOR = (color: string): UpdateCallback => {
+        return (bubble: Bubble): void => {
+            bubble.color = color;
+        }
     }
 
     render(canvas: HTMLCanvasElement): void {
         const ctx = canvas.getContext("2d");
-        ctx.fillStyle = this.color(this.isOverlapping);
+        ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2, true);
     }
 
+    isOverlapping(bubble: Bubble): boolean {
+        return this.pos.dist(bubble.pos) <= this.radius + bubble.radius;
+    }
 }
--- BubblesLab/src/main.ts
+++ BubblesLab/src/main.ts
@@ -1,4 +1,5 @@
-import Vec2D from "./vec2d";
+import Vec2D from "./vec2d.js";
+import Bubble from "./bubble.js";
 
 window.addEventListener('load', init);
 
@@ -13,6 +14,12 @@ function init() {
     resiveCanvas(canvas);
     window.addEventListener('resize', () => resiveCanvas(canvas));
     const ctx = canvas.getContext("2d");
-
-    const vec = new Vec2D();
+    
+    const width = canvas.width, height = canvas.height, scale = width / 1920;
+    const bubbles = Array<Bubble>(200);
+    for (let i = 0; i < 200; ++i) {
+        const pos = new Vec2D(Math.random() * width, Math.random() * height),
+              vel = new Vec2D((Math.random() - 0.5) * 10 * scale, (Math.random() - 0.5) * 10 * scale),
+              acc = new Vec2D(); // TODO:
+    }
 }
