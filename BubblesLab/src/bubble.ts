02012bf1be1c874c60e79ab786dcddec807ba688
Diego Comfort <diegocomfort@gmail.com>
2023-09-11 19:47:54 -0700

    Created a builder class and applyForces()

--- BubblesLab/src/bubble.ts
+++ BubblesLab/src/bubble.ts
@@ -8,13 +8,13 @@ export default class Bubble {
     private overlapping_: boolean = false;
     private defaultColor_: string;
     private overlapColor_: string;
-    
+
     constructor(radius: number,
-                pos: Vec2D = new Vec2D, 
-                vel: Vec2D = new Vec2D, 
-                acc: Vec2D = new Vec2D,
-                defaultColor: string = "white",
-                overlapColor: string = "pink") {
+        pos: Vec2D = new Vec2D,
+        vel: Vec2D = new Vec2D,
+        acc: Vec2D = new Vec2D,
+        defaultColor: string = "white",
+        overlapColor: string = "pink") {
         this.radius_ = radius;
         this.pos_ = pos;
         this.vel_ = vel;
@@ -23,6 +23,60 @@ export default class Bubble {
         this.overlapColor_ = overlapColor;
     }
 
+    public Builder = class Builder {
+        private radius: number;
+        private pos: Vec2D;
+        private vel: Vec2D;
+        private acc: Vec2D;
+        private defaultClr: string;
+        private overlapClr: string;
+
+        constructor(radius: number) {
+            this.radius = radius;
+            this.pos = new Vec2D();
+            this.vel = new Vec2D();
+            this.acc = new Vec2D();
+            this.defaultClr = "black";
+            this.overlapClr = "white";
+        }
+
+        build(): Bubble {
+            return new Bubble(
+                this.radius,
+                this.pos,
+                this.vel,
+                this.acc,
+                this.defaultClr,
+                this.overlapClr,
+            );
+        }
+
+        position(pos: Vec2D): Builder {
+            this.pos = pos;
+            return this;
+        }
+
+        velocity(vel: Vec2D): Builder {
+            this.vel = vel;
+            return this;
+        }
+
+        acceleration(acc: Vec2D): Builder {
+            this.acc = acc;
+            return this;
+        }
+
+        defaultColor(clr: string): Builder {
+            this.defaultClr = clr;
+            return this;
+        }
+
+        overlapColor(clr: string): Builder {
+            this.overlapClr = clr;
+            return this;
+        }
+    };
+
     public get radius(): number {
         return this.radius_;
     }
@@ -30,7 +84,7 @@ export default class Bubble {
     public get pos(): Vec2D {
         return this.pos_;
     }
-    
+
     public get vel(): Vec2D {
         return this.vel_;
     }
@@ -51,13 +105,36 @@ export default class Bubble {
         return this.overlapColor_;
     }
 
+    applyForces(attractors: Array<Bubble>, repulsers: Array<Bubble>): Vec2D {
+        let acc = new Vec2D();
+        const scale = 0.5;
+
+        for (const attractor of attractors) {
+            let tmp = attractor.pos
+                .sub(this.pos)
+                .normalize()
+                .mult(scale);
+            acc = acc.add(tmp);
+        }
+
+        for (const repulser of repulsers) {
+            let tmp = this.pos
+                .sub(repulser.pos)
+                .normalize()
+                .mult(scale);
+            acc = acc.add(tmp);
+        }
+
+        return acc;
+    }
+
     update(deltaTime: number, newAccelaration: Vec2D = new Vec2D): void {
         // Verlet integration: https://en.wikipedia.org/wiki/Verlet_integration
         this.pos_ = this.pos_
-                            .add(this.vel_.mult(deltaTime))
-                            .add(this.acc_.mult(deltaTime ** 2 * 0.5));
+            .add(this.vel_.mult(deltaTime))
+            .add(this.acc_.mult(deltaTime ** 2 * 0.5));
         this.vel_ = this.vel_
-                            .add(this.acc_.add(newAccelaration).mult(deltaTime * 0.5));
+            .add(this.acc_.add(newAccelaration).mult(deltaTime * 0.5));
         this.acc_ = newAccelaration;
     }
 
@@ -65,7 +142,7 @@ export default class Bubble {
         if (this.pos_.x - this.radius_ < 0) {
             this.pos_ = this.pos_.setX((x) => this.radius_);
             this.vel_ = this.vel_.setX((x) => -x);
-        }else if (this.pos_.x + this.radius_ > width) {
+        } else if (this.pos_.x + this.radius_ > width) {
             this.pos_ = this.pos_.setX((x) => width - this.radius_);
             this.vel_ = this.vel_.setX((x) => -x);
             console.log(Date.now());
