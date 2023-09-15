8862a359e84a680b2a3c7ac80166958074fee9ae
Diego Comfort <diegocomfort@gmail.com>
2023-09-14 21:09:51 -0700

    Corrected importof Utils

---
+++ Lab0914_Movers_Forces/src/main.ts
@@ -0,0 +1,27 @@
+import { Utils } from "./utils.js"
+
+window.addEventListener('load', init);
+
+function init() {
+    const game = {
+        canvas: window.document.querySelector('canvas')!,
+        time: 0,
+    };
+
+    Utils.resizeCanvas(game.canvas);
+
+    game.time = Date.now();
+    animate(game);
+}
+
+function animate(game: { canvas: HTMLCanvasElement; time: number; }) {
+    const currentTime = Date.now();
+    const dt = (currentTime - game.time) / 1000; // Delta time bewteen last 'frame' in seconds
+    game.time = currentTime;
+
+    const ctx = game.canvas.getContext("2d")!;
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
+
+    requestAnimationFrame(() => animate(game));
+}
