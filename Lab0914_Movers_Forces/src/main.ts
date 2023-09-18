fa9ac5c71ccd3a8ec50e5bd3afdf04daec749919
DiegoComfort <diegocomfort@gmail.com>
2023-09-18 15:17:46 -0700

    Updated for 3 force types and to fit new Mover structure

--- Lab0914_Movers_Forces/src/main.ts
+++ Lab0914_Movers_Forces/src/main.ts
@@ -9,29 +9,20 @@ function init() {
         canvas: window.document.querySelector('canvas')!,
         time: 0,
         movers: new Array<Mover>,
-        gravitor: new Mover({ radius: 0 }),
-        attract: false,
+        gravity: 0,
     };
     
-    game.canvas.addEventListener("mousedown", () => game.attract = !game.attract);
+    game.canvas.addEventListener("mousedown", () => game.gravity = Utils.mod(game.gravity + 1, 3));
 
     Utils.resizeCanvas(game.canvas);
 
-    game.gravitor = new Mover({
-        radius: 50,
-        position: new Vec2D(game.canvas.width / 2, game.canvas.height / 2),
-        velocity: Vec2D.fromAngle(Math.random() * Math.PI * 2, Math.random() * 30 + 30),
-    });
-
-    for (let i = 0; i < 1000; ++i) {
+    for (let i = 0; i < 100; ++i) {
         game.movers.push(new Mover({
-            radius: Math.random() * 20 + 10,
+            radius: Math.random() ** 10 * 75 + 5,
             position: new Vec2D(game.canvas.width * Math.random(), game.canvas.height * Math.random()),
             velocity: Vec2D.fromAngle(Math.random() * Math.PI * 2, Math.random() * 30 + 30)
         }))
     }
-
-    
 
     game.time = Date.now();
     animate(game);
@@ -41,36 +32,38 @@ function animate(game: {
     canvas: HTMLCanvasElement;
     time: number;
     movers: Mover[];
-    gravitor: Mover;
-    attract: boolean;
+    gravity: number;
 }) {
     const currentTime = Date.now();
     const dt = (currentTime - game.time) / 1000; // Delta time bewteen last 'frame' in seconds
     game.time = currentTime;
 
     const ctx = game.canvas.getContext("2d")!;
-    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    ctx.fillStyle = "rgba(255, 255, 255, 0.05)";
     ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
 
-    game.gravitor.update(dt);
-    game.gravitor.checkWalls(game.canvas.width, game.canvas.height, false);
-    game.gravitor.render(game.canvas, "red");
 
     for (const mover of game.movers) {
 
-        const newAcc = (game.attract
-            ? game.gravitor.position.sub(mover.position)
-            : mover.position.sub(game.gravitor.position))
-            .normalize()
-            .mult(game.gravitor.mass * 10)
-            .div(mover.position.dist(game.gravitor.position));
-        mover.update(dt, newAcc);
-        mover.limitVelocity(50);
-        mover.checkWalls(game.canvas.width, game.canvas.height);
-        mover.render(game.canvas);
-    }
+        let acc = mover.applyForces(game.movers);
+        if (game.gravity === 1)                // Gravity
+            acc = Vec2D.J.mult(9.80665e+1);
+        if (game.gravity === 2)                // Wind
+            acc = Vec2D.I.mult(9.80665e+1);
 
-    console.log(game.movers[0].velocity.magSq());
+        mover.update(dt, acc);
+        if (game.gravity !== 1)
+            mover.limitVelocity(50);
+        else 
+            mover.limitVelocity(200);
+
+        mover.checkWalls(game.canvas.width, game.canvas.height, game.gravity == 2 ? false: true);
+        mover.checkMovers(game.movers);
+        mover.render(game.canvas);
+
+        if (mover === game.movers[0])
+            console.log(mover.velocity)
+    }
 
     requestAnimationFrame(() => animate(game));
 }
