981acb634cad77ea7dcff4788c272bb0a6bdcee1
Diego Comfort <diegocomfort@gmail.com>
2023-09-27 23:40:50 -0700

    Made all properties public

--- Lab0926_Interactions/src/body.ts
+++ Lab0926_Interactions/src/body.ts
@@ -1,32 +1,12 @@
 import System from "./system.js";
 import Vec2D from "./vec2d.js";
 
-export default class Body implements System{
-    private _position: Vec2D;
-    private _velocity: Vec2D;
-    private _acceleration: Vec2D;
-    private _mass: number;
-    private _radius: number;
-
-    public get position(): Vec2D {
-        return this._position;
-    }
-
-    public get velocity(): Vec2D {
-        return this._velocity;
-    }
-
-    public get acceleration(): Vec2D {
-        return this._acceleration;
-    }
-
-    public get mass(): number {
-        return this._mass;
-    }
-
-    public get radius(): number {
-        return this._radius;
-    }
+export default class Body implements System {
+    position: Vec2D;
+    velocity: Vec2D;
+    acceleration: Vec2D;
+    mass: number;
+    radius: number;
 
     constructor(params: {
         radius: number;
@@ -35,18 +15,18 @@ export default class Body implements System{
         velocity?: Vec2D;
         acceleration?: Vec2D;
     }) {
-        this._radius = params.radius;
-        this._mass = params.mass ?? Math.PI * this._radius ** 2;
-        this._position = params.position ?? new Vec2D();
-        this._velocity = params.velocity ?? new Vec2D();
-        this._acceleration = params.acceleration ?? new Vec2D();
+        this.radius = params.radius;
+        this.mass = params.mass ?? Math.PI * this.radius ** 2;
+        this.position = params.position ?? new Vec2D();
+        this.velocity = params.velocity ?? new Vec2D();
+        this.acceleration = params.acceleration ?? new Vec2D();
     }
 
     update(
         deltaTime: number,
         environment: ReadonlyArray<Readonly<System>>,
         envWidth: number,
-        envHeight: number,
+        envHeight: number
     ): void {
         // TODO:
     }
