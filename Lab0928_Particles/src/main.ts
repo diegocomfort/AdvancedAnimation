5f89f53e1ebc1fd4f9fccf2b93ef447a6acf9c7f
Diego Comfort <diegocomfort@gmail.com>
2023-09-28 19:02:59 -0700

    Initialized new Particles lab

---
+++ Lab0928_Particles/src/main.ts
@@ -0,0 +1,69 @@
+import Body from "./systems/body.js";
+import BodyWithSatellites from "./systems/bodywithsatellites.js";
+import LinkingBody from "./systems/linkingbody.js";
+import System from "./system.js";
+import { TAU, resizeCanvas } from "./utils.js";
+import Vec2D from "./vec2d.js";
+
+window.addEventListener("load", init);
+
+function init() {
+    const game = {
+        canvas: window.document.querySelector("canvas")!,
+        time: 0,
+        env: new Array<System>(),
+    };
+
+    resizeCanvas(game.canvas);
+
+    const n = 10;
+    for (let i = 0; i < n; ++i) {
+        game.env.push(
+            new LinkingBody({
+                radius: 40,
+                position: new Vec2D(
+                    Math.random() * game.canvas.width,
+                    Math.random() * game.canvas.height
+                ),
+                velocity: Vec2D.fromAngle(Math.random() * Math.PI * 2, Math.random() * 50),
+            }, {
+                radius: 10,
+                amount: 4,
+                color: "red",
+                angularVelocity: TAU / 20
+            })
+        );
+    }
+
+    game.time = Date.now();
+    console.log(game);
+    animate(game);
+}
+
+function animate(game: {
+    canvas: HTMLCanvasElement;
+    time: number;
+    env: System[];
+}) {
+    const currentTime = Date.now();
+    const dt = (currentTime - game.time) * 1e-3; // Delta time bewteen last 'frame' in seconds
+    game.time = currentTime;
+
+    const ctx = game.canvas.getContext("2d")!;
+    ctx.fillStyle = "rgba(255, 255, 255, 0.025)";
+    ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
+
+    const n = 10;
+    for (let i = 0; i < n; ++i) {
+        for (const mover of game.env) {
+            mover.update(dt / n, game.env);
+        }
+    }
+
+    for (const mover of game.env) {
+        mover.applyBehaviors(game.env, game.canvas.width, game.canvas.height)
+        mover.render(game.canvas);
+    }
+
+    requestAnimationFrame(() => animate(game));
+}
---
+++ Lab0928_Particles/src/system.ts
@@ -0,0 +1,39 @@
+import Vec2D from "./vec2d.js";
+
+export default interface System {
+    /**
+     * Update this System
+     * @param deltaTime the time, in seconds, between the start of the current frame and the start of the previous
+     * @param environment the environment that this System belongs to
+     */
+    update(
+        deltaTime: number,
+        environment: ReadonlyArray<Readonly<System>>
+    ): void;
+
+    /**
+     * Change the environment if certain behaviors should occur
+     * @param environment the environment that this System belongs to
+     */
+    applyBehaviors(
+        environment: Array<System>,
+        envWidth: number,
+        envHeight: number
+    ): void;
+
+    /**
+     * Render this System
+     * @param canvas the canvas to render this onto
+     */
+    render(canvas: HTMLCanvasElement): void;
+
+    /**
+     * Get the total mass of this System
+     */
+    get mass(): number;
+
+    /**
+     * Get the position (or average position) of this System
+     */
+    get position(): Vec2D;
+}
