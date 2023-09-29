550148468c2ddfb22080feebff7b7d3f24a8c253
Diego Comfort <diegocomfort@gmail.com>
2023-09-28 20:46:17 -0700

    Updated Color class

--- Lab0928_Particles/src/utils.ts
+++ Lab0928_Particles/src/utils.ts
@@ -11,16 +11,29 @@ export function mod(a: number, b: number): number {
 export const TAU = Math.PI * 2;
 
 export class Color {
-    r: number;
-    g: number;
-    b: number;
-    a: number
+    readonly r: number;
+    readonly g: number;
+    readonly b: number;
+    readonly a: number
 
     constructor(r?: number, g?: number, b?: number, a?: number) {
         this.r = r ?? 0;
         this.g = g ?? 0;
         this.b = b ?? 0;
         this.a = a ?? 1;
+    }
+
+    setR(r: number): Color {
+        return new Color(r, this.g, this.b, this.a);
+    }
+    setG(g: number): Color {
+        return new Color(this.r, g, this.b, this.a);
+    }
+    setB(b: number): Color {
+        return new Color(this.r, this.g, b, this.a);
+    }
+    setA(a: number): Color {
+        return new Color(this.r, this.g, this.b, a);
     }
 
     toString(): string {
