841b79cd9b0383aae5e1b05e4f973cdb26c40ad1
Diego Comfort <diegocomfort@gmail.com>
2023-09-28 20:40:04 -0700

    Added simple Color class

--- Lab0928_Particles/src/utils.ts
+++ Lab0928_Particles/src/utils.ts
@@ -9,3 +9,21 @@ export function mod(a: number, b: number): number {
 }
 
 export const TAU = Math.PI * 2;
+
+export class Color {
+    r: number;
+    g: number;
+    b: number;
+    a: number
+
+    constructor(r?: number, g?: number, b?: number, a?: number) {
+        this.r = r ?? 0;
+        this.g = g ?? 0;
+        this.b = b ?? 0;
+        this.a = a ?? 1;
+    }
+
+    toString(): string {
+        return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
+    }
+}
